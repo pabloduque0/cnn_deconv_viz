0d4108443780f545b18cf0ab162e7f7797acac55
Pablo Duque <menduke@live.com>
2018-12-10 17:27:43 +0100

    Added crop shapes function to basenetwork

--- basenetwork.py
+++ basenetwork.py
@@ -25,6 +25,23 @@ class BaseNetwork():
         self.model = model
         self.full_paths_dict = None
 
+    def get_crop_shape(self, target, refer):
+        # width, the 3rd dimension
+        cw = (target.get_shape()[2] - refer.get_shape()[2]).value
+        assert (cw >= 0)
+        if cw % 2 != 0:
+            cw1, cw2 = int(cw / 2), int(cw / 2) + 1
+        else:
+            cw1, cw2 = int(cw / 2), int(cw / 2)
+        # height, the 2nd dimension
+        ch = (target.get_shape()[1] - refer.get_shape()[1]).value
+        assert (ch >= 0)
+        if ch % 2 != 0:
+            ch1, ch2 = int(ch / 2), int(ch / 2) + 1
+        else:
+            ch1, ch2 = int(ch / 2), int(ch / 2)
+
+        return (ch1, ch2), (cw1, cw2)
 
     def save_specs(self, specs_path, fit_specs):
 
