64a492428e7603b5a4f05e35bcaf50fa38e99fa7
Pablo Duque <menduke@live.com>
2019-03-01 19:30:54 +0100

    Minor change

--- runner.py
+++ runner.py
@@ -187,7 +187,7 @@ base_path = os.getcwd()
 
 print(data_train.shape, labels_train.shape, test_data.shape, test_labels.shape)
 unet = OldUnet(img_shape=data_train.shape[1:])
-unet.train(data_train, labels_train, (test_data, test_labels), training_name, base_path, epochs=1, batch_size=30)
+unet.train(data_train, labels_train, (test_data, test_labels), training_name, base_path, epochs=35, batch_size=30)
 
 '''
 
