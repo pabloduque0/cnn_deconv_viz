69b822b003b42e667cf88dc79638eaa6e9021d17
Pablo Duque <menduke@live.com>
2019-03-27 19:17:29 +0100

    Added deconv as network in runner

--- runner.py
+++ runner.py
@@ -186,7 +186,7 @@ training_name = 'test_new_deconv'
 base_path = os.getcwd()
 
 print(data_train.shape, labels_train.shape, test_data.shape, test_labels.shape)
-unet = OldUnet(img_shape=data_train.shape[1:])
+unet = UnetDeconv(img_shape=data_train.shape[1:])
 unet.train(data_train, labels_train, (test_data, test_labels), training_name, base_path, epochs=35, batch_size=30)
 
 '''
@@ -205,4 +205,4 @@ VISUALIZING
 """
 del data_train, labels_train, training_name
 gc.collect()
-#unet.save_visualize_activations(validation_data, validation_labels, batch_size=30)
+unet.save_visualize_activations(validation_data, validation_labels, batch_size=30)
