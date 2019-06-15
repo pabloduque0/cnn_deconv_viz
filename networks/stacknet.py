dea39acdb9f3b19fee29b1ca5295f7cfb8082bd9
Pablo Duque <menduke@live.com>
2019-06-15 19:28:59 +0200

    minor change in param input

--- networks/stacknet.py
+++ networks/stacknet.py
@@ -27,101 +27,101 @@ class StackNet(BaseNetwork):
         super().__init__(model, img_shape)
 
 
-    def create_model(self, img_shape):
+    def create_model(self, img_shape, kernel_size=5):
         concat_axis = 3
 
         inputs = layers.Input(shape=img_shape)
 
-        stack1 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        stack1 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             inputs)
-        stack2 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        stack2 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             stack1)
-        stack3 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        stack3 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             stack2)
 
-        conv1 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv1 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             stack3)
-        conv2 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv2 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv1)
         #conv2 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv2)
 
         maxpool1 = layers.MaxPool2D(pool_size=(2, 2))(conv2)
 
-        conv3 = layers.Conv2D(80, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv3 = layers.Conv2D(80, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             maxpool1)
-        conv4 = layers.Conv2D(80, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv4 = layers.Conv2D(80, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv3)
         #conv4 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv4)
         maxpool2 = layers.MaxPool2D(pool_size=(2, 2))(conv4)
 
-        conv5 = layers.Conv2D(112, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv5 = layers.Conv2D(112, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             maxpool2)
-        conv6 = layers.Conv2D(112, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv6 = layers.Conv2D(112, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv5)
         #conv6 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv6)
         maxpool3 = layers.MaxPool2D(pool_size=(2, 2))(conv6)
 
-        conv7 = layers.Conv2D(224, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv7 = layers.Conv2D(224, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             maxpool3)
         conv8 = layers.Conv2D(224, kernel_size=3, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv7)
         #conv8 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv8)
         maxpool4 = layers.MaxPool2D(pool_size=(2, 2))(conv8)
 
-        conv9 = layers.Conv2D(224, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv9 = layers.Conv2D(224, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             maxpool4)
-        conv10 = layers.Conv2D(416, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv10 = layers.Conv2D(416, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv9)
         #conv10 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv10)
         up_conv10 = layers.UpSampling2D(size=(2, 2))(conv10)
-        up_conv1 = layers.Conv2D(416, kernel_size=5, padding='same',
+        up_conv1 = layers.Conv2D(416, kernel_size=kernel_size, padding='same',
                                  kernel_initializer='he_normal', activation='relu')(up_conv10)
         ch, cw = self.get_crop_shape(conv8, up_conv1)
         crop_conv8 = layers.Cropping2D(cropping=(ch, cw))(conv8)
         up_samp1 = layers.concatenate([crop_conv8, up_conv1], axis=concat_axis)
 
-        conv11 = layers.Conv2D(224, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv11 = layers.Conv2D(224, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             up_samp1)
-        conv12 = layers.Conv2D(224, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv12 = layers.Conv2D(224, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv11)
         #conv12 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv12)
 
         up_conv12 = layers.UpSampling2D(size=(2, 2))(conv12)
-        up_conv2 = layers.Conv2D(336, kernel_size=5, padding='same',
+        up_conv2 = layers.Conv2D(336, kernel_size=kernel_size, padding='same',
                                  kernel_initializer='he_normal', activation='relu')(up_conv12)
         ch, cw = self.get_crop_shape(conv6, up_conv2)
         crop_conv6 = layers.Cropping2D(cropping=(ch, cw))(conv6)
         up_samp2 = layers.concatenate([crop_conv6, up_conv2], axis=concat_axis)
 
-        conv13 = layers.Conv2D(112, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv13 = layers.Conv2D(112, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             up_samp2)
-        conv14 = layers.Conv2D(112, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv14 = layers.Conv2D(112, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv13)
         #conv14 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv14)
 
         up_conv14 = layers.UpSampling2D(size=(2, 2))(conv14)
-        up_conv3 = layers.Conv2D(192, kernel_size=5, padding='same',
+        up_conv3 = layers.Conv2D(192, kernel_size=kernel_size, padding='same',
                                  kernel_initializer='he_normal', activation='relu')(up_conv14)
         ch, cw = self.get_crop_shape(conv4, up_conv3)
         crop_conv4 = layers.Cropping2D(cropping=(ch, cw))(conv4)
         up_samp3 = layers.concatenate([crop_conv4, up_conv3], axis=concat_axis)
 
-        conv15 = layers.Conv2D(80, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv15 = layers.Conv2D(80, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             up_samp3)
-        conv16 = layers.Conv2D(80, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv16 = layers.Conv2D(80, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv15)
         #conv16 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv16)
 
         up_conv16 = layers.UpSampling2D(size=(2, 2))(conv16)
-        up_conv4 = layers.Conv2D(140, kernel_size=5, padding='same',
+        up_conv4 = layers.Conv2D(140, kernel_size=kernel_size, padding='same',
                                  kernel_initializer='he_normal', activation='relu')(up_conv16)
         ch, cw = self.get_crop_shape(conv2, up_conv4)
         crop_conv2 = layers.Cropping2D(cropping=(ch, cw))(conv2)
         up_samp4 = layers.concatenate([crop_conv2, up_conv4], axis=concat_axis)
 
-        conv21 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv21 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             up_samp4)
-        conv22 = layers.Conv2D(40, kernel_size=5, padding='same', kernel_initializer='he_normal', activation='relu')(
+        conv22 = layers.Conv2D(40, kernel_size=kernel_size, padding='same', kernel_initializer='he_normal', activation='relu')(
             conv21)
         #conv22 = layers.BatchNormalization(axis=concat_axis, momentum=0.99, epsilon=0.001, center=True, scale=True)(conv22)
 
