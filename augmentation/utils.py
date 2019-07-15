97a3ce942cb94bebfcfc25c286d5fb951f3e3545
Pablo Duque <menduke@live.com>
2019-07-16 00:06:42 +0200

    Minor fix for last group

--- augmentation/utils.py
+++ augmentation/utils.py
@@ -3,8 +3,10 @@ import numpy as np
 
 def make_indices_groups(images, size_group=None, n_groups=None):
 
-    if size_group is None and n_groups is None:
-        raise Exception("Either size_group or n_groups must be passed to generate indices groups.")
+    if size_group is None:
+        if n_groups is None:
+            raise Exception("Either size_group or n_groups must be passed to generate indices groups.")
+        size_group = len(images) // n_groups
 
     if n_groups is None:
         n_groups = len(images) // size_group
@@ -16,8 +18,6 @@ def make_indices_groups(images, size_group=None, n_groups=None):
         if i < n_groups - 1:
             group = np.random.choice(list(indices_left), size_group)
             indices_left = indices_left.difference(set(group))
-        else:
-            group = np.random.choice(list(indices_left), len(indices_left))
 
         list_groups.append(group)
 
