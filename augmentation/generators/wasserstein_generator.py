5165ee744bba2244203cbbd8705c198cde06defe
Pablo Duque <menduke@live.com>
2019-07-21 11:18:47 +0200

    Fixed size matches

--- augmentation/generators/wasserstein_generator.py
+++ augmentation/generators/wasserstein_generator.py
@@ -18,18 +18,27 @@ def create_model(input_shape, sub_start_shape=(6, 6, 128)):
     print("prior shape: ", K.int_shape(lr_2))
     reshaped = layers.Reshape(sub_start_shape, input_shape=(np.prod(list(sub_start_shape)),))(lr_2)
     bn_axis = -1
-    conv_1 = layers.Conv2DTranspose(128, (5, 5), strides=2, padding='same')(reshaped)
+    conv_1 = layers.Conv2DTranspose(512, (5, 5), strides=2, padding='same')(reshaped)
     bn_2 = layers.BatchNormalization(axis=bn_axis)(conv_1)
     lr_3 = layers.LeakyReLU()(bn_2)
-    conv_2 = layers.Convolution2D(64, (5, 5), padding='same')(lr_3)
+
+    conv_2 = layers.Conv2DTranspose(256, (5, 5), strides=2, padding='same')(lr_3)
     bn_3 = layers.BatchNormalization(axis=bn_axis)(conv_2)
     lr_4 = layers.LeakyReLU()(bn_3)
-    conv_3 = layers.Conv2DTranspose(64, (5, 5), strides=2, padding='same')(lr_4)
+
+    padded = layers.ZeroPadding2D(padding=((1, 0), (1, 0)))(lr_4)
+
+    conv_3 = layers.Conv2DTranspose(128, (5, 5), strides=2, padding='same')(padded)
     bn_5 = layers.BatchNormalization(axis=bn_axis)(conv_3)
     lr_6 = layers.LeakyReLU()(bn_5)
-    conv_4 = layers.Convolution2D(2, (5, 5), padding='same', activation='tanh')(lr_6)
 
-    model = models.Model(inputs=input_layer, outputs=conv_4)
+    conv_4 = layers.Conv2DTranspose(64, (5, 5), strides=2, padding='same')(lr_6)
+    bn_6 = layers.BatchNormalization(axis=bn_axis)(conv_4)
+    lr_7 = layers.LeakyReLU()(bn_6)
+
+    conv_5 = layers.Conv2DTranspose(2, (5, 5), strides=2, padding='same')(lr_7)
+
+    model = models.Model(inputs=input_layer, outputs=conv_5)
     model.summary()
     return model
 
