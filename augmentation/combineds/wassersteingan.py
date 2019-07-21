4243acfc549a59539ca08dbc35b997eeff209c1f
Pablo Duque <menduke@live.com>
2019-07-21 12:20:06 +0200

    Fixes in wass gan

--- augmentation/combineds/wassersteingan.py
+++ augmentation/combineds/wassersteingan.py
@@ -16,12 +16,10 @@ class WassersteinGAN(MotherGAN):
         discriminator_model = wasserstein_discriminator.create_model(img_shape)
         # Build and compile the discriminator
         discriminator_model.compile(optimizers.Adam(lr=0.00001, beta_1=0, beta_2=0.99),
-                                   loss=losses.binary_crossentropy, metrics=['accuracy'])
+                                    loss=metrics.wasserstein_loss, metrics=['accuracy'])
 
         # Build and compile the generator
         generator_model = wasserstein_generator.create_model(noise_shape)
-        generator_model.compile(optimizers.Adam(lr=0.00001, beta_1=0, beta_2=0.99),
-                               loss=losses.binary_crossentropy)
 
         # The generator takes noise as input and generated imgs
         z = layers.Input(shape=noise_shape)
@@ -35,6 +33,11 @@ class WassersteinGAN(MotherGAN):
         # The combined model  (stacked generator and discriminator) takes
         # noise as input => generates images => determines validity
         combined_model = models.Model(z, valid)
-        combined_model.compile(optimizers.Adam(lr=0.00001, beta_1=0, beta_2=0.99), loss=metrics.generator_loss)
+        combined_model.compile(optimizers.Adam(lr=0.00001, beta_1=0, beta_2=0.99), loss=metrics.wasserstein_loss)
 
         super().__init__(generator_model, discriminator_model, combined_model, img_shape, noise_shape)
+
+
+
+
+
