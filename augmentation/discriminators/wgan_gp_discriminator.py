fa0f2a798eadd7ad871db940c1406e55c7ba0f2d
Pablo Duque <menduke@live.com>
2019-07-30 00:44:53 +0200

    First version of wgan gp resnet critic

---
+++ augmentation/discriminators/wgan_gp_discriminator.py
@@ -0,0 +1,35 @@
+from keras import models
+from keras import layers
+import tensorflow as tf
+from keras.optimizers import Adam
+from augmentation import metrics
+from keras import losses
+import keras.backend as K
+
+
+def create_model(input_shape):
+
+    input_layer = layers.Input(shape=input_shape)
+
+    res_out_1 = residual_block(input_layer, 3, True)
+    res_out_2 = residual_block(res_out_1, 3, True)
+    res_out_3 = residual_block(res_out_2, 3, False)
+    res_out_4 = residual_block(res_out_3, 3, True)
+    flatten = layers.Flatten()(res_out_4)
+    dense_1 = layers.Dense(1, kernel_initializer='he_normal')(flatten)
+
+    model = models.Model(inputs=input_layer, outputs=dense_1)
+    model.summary()
+
+    return model
+
+
+def residual_block(input_layer, k_sizes, down_sample):
+
+    conv_1 = layers.Convolution2D(64, k_sizes, padding='same', activation="relu")(input_layer)
+    conv_2 = layers.Convolution2D(64, k_sizes, padding='same', activation="relu")(conv_1)
+    if down_sample:
+        conv_2 = layers.AveragePooling2D(pool_size=(2, 2))(conv_2)
+
+    res = layers.Add()([conv_2, input_layer])
+    return res
